3736368c23d3ddd6bd9e37dab262db3101fc7a60
Nenouchka <helene.morel@gmail.com>
2020-04-15 16:18:58 +0200

    début code counter

---
+++ src/components/Counter.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Counter.css'
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+
+    increment = () => {
+    // ajouter algo lié aux cartes : 1 essai = 2 cartes retournées
+        this.setState ({
+            count: this.state.count +1
+        })
+    }
+
+    // en cas de bonus ?
+    decrement = () => {
+        this.setState ({
+            count: this.state.count -1
+        })
+    }
+
+    render() {
+        return(
+            <>
+                <h3>Your moves are watched</h3>
+                <p className= "counter">{this.state.count}</p>
+            </>
+        )
+    }
+}
+
+
+
+export default Counter
