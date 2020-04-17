6654a7b7f866e6c906944998c2d9faca0a5ef147
Nenouchka <helene.morel@gmail.com>
2020-04-17 12:22:16 +0200

    suppression code et fichier police  inutiles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './fonts/CrucifixionRegular-xKd8.ttf'
 import './fonts/MyHardboardCut-JRmaK.otf'
 
 ReactDOM.render(
