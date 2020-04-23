b8479c33e2e5a4da51b6c261d8071cbe09cd9182
kphiri84 <kenny.phiri@laposte.net>
2020-04-23 12:28:17 +0200

    modificaiton of the app.js and the component videoplayer

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
 import VideoPlayer from './components/VideoPlayer';
-import axios from 'axios';
 
-const App = () =>{
-  const [player, setPlayer] = useState([])
 
-  useEffect(() =>{
-    getVideo()
-  }, [])
-
-  const getVideo = () =>{
-      axios.get("https://horrormemo.herokuapp.com/memory")
-        .then(response => setPlayer(response.data))
-    }
-    console.log(player)
-    return (
-      <div className="App">
-        {player.filter(player => player.id <= 19)
-        .map(player =>
-          <VideoPlayer name={player.name} video={player.video}/>
-        )}
-      </div>
-    );
-  
+function App() {
+  return (
+    <>
+      <VideoPlayer />
+    </>
+  );
 }
-
 export default App;
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import ReactPlayer from 'react-player'
+import axios from 'axios';
 
-function VideoPlayer ({name,video}) {
+const VideoPlayer =() =>{
+    const [player, setPlayer] = useState([])
+
+  useEffect(() =>{
+    getVideo()
+  }, [])
+
+  const getVideo = () =>{
+      axios.get("https://horrormemo.herokuapp.com/memory")
+        .then(response => setPlayer(response.data))
+    }
+
     return(
     <div>
-    <h4>{name}</h4>
-    <ReactPlayer url={video} 
-     light
-     controls = 'true'
-     width = '45%'
-     />
-    
-
+        {player.filter(player => player.id <= 19)
+        .map(player =>
+            <div>
+            <h4>{player.name}</h4>
+            <ReactPlayer url={player.video} 
+            light
+            controls = 'true'
+            width = '45%'/>
+            </div>
+        )}
     </div>
-    
     )
 }
 
