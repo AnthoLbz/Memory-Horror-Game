6870c4fe83a7737a1134b310dbb5584f9719957e
kphiri84 <kenny.phiri@laposte.net>
2020-04-26 12:08:24 +0200

    creation of the movie page

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
+import './VideoPlayer.css';
 import ReactPlayer from 'react-player'
 import axios from 'axios';
+import GlitchClip from 'react-glitch-effect/core/Clip';
 
 const VideoPlayer =() =>{
     const [player, setPlayer] = useState([])
@@ -15,17 +17,31 @@ const VideoPlayer =() =>{
     }
 
     return(
-    <div>
+      <div className="Body1">
+      <GlitchClip>
+        <h1 className="Title">Movies Informations</h1>
+      </GlitchClip>
+      <div className="Body">
         {player.filter(player => player.id <= 19)
         .map(player =>
-            <div>
-            <h4>{player.name}</h4>
-            <ReactPlayer url={player.video} 
-            light
-            controls = 'true'
-            width = '45%'/>
+          <div className="test">
+            <div className="VideoPlayer">
+              <img className="Card" src={player.image} alt={player.name} />
+              <GlitchClip className="Description">
+              <h4 className="Description">{player.name}</h4>
+              </GlitchClip>
+              <ReactPlayer className ="Player" 
+              url={player.video} 
+              controls = 'true'
+              width = '30%'
+              height = '210px'/>
             </div>
+            <div>
+              <hr />
+            </div> 
+          </div>
         )}
+      </div>
     </div>
     )
 }
