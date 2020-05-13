df39ef6222fa434bfdecd97c5e85313e8a7bb51f
AnthoLbz <anthony.lasbleiz@gmail.com>
2020-05-13 18:10:52 +0200

    started the contact page + minor tweaks

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import "./contact.css";
-
+import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
     <div className="contact">
-      <p> mdsfgjdfmsgj</p>
+      <Link to="/" className="contactTitle">
+        Memory Horror Game
+      </Link>
+      <h2 className="contactContainer">
+        <p>
+          This game was brought to you by 4 Wild Code School Marseille students.
+        </p>
+        <p>
+          In the event you wish to contact them, the following information may
+          help you.
+        </p>
+        <p>But careful, no complaints allowed !</p>
+      </h2>
     </div>
   );
 };
